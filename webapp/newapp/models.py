60e9fd35dc9fd7ae722bdcf4b5f4d1555923a64d
Va8661 <vaishnav861999@gmail.com>
2023-03-17 10:35:17 +0530

    secon commit

--- webapp/newapp/models.py
+++ webapp/newapp/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Appointment(models.Model):
-    firstName = models.CharField(max_length=30, default='')
-    lastName = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=200, default='')
     age = models.PositiveIntegerField(default=0)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
     date = models.DateField()
     time = models.TimeField()
     symptoms = models.TextField(default='')
-    name = models.CharField(max_length=200, default='')
     token_number = models.IntegerField()
 
     def __str__(self):
