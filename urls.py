5320f48dfb5786cc9e2bad149d53d8de7d03fcf6
Vai <vaishnav861999@gmail.com>
2023-03-23 15:07:03 +0530

    second commit

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.hi, name='home-page' ),
-     path('book_appointment/', views.book_appointment, name='book_appointment'),
-]
